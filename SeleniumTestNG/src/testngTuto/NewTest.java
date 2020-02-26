645139845d11a4b4e18f235d3b2b8a85c90a5d1f
Yogesh Kamra <Yogesh Kamra@DESKTOP-NRDHTR2>
2020-02-26 12:25:16 -0600

    TestNG Selenium

---
+++ SeleniumTestNG/src/testngTuto/NewTest.java
@@ -0,0 +1,43 @@
+package testngTuto;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	
+	@BeforeSuite
+	public void beforeset() {
+		
+		System.out.println("Before suite check all requirements");
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		
+		System.out.println("Before class you need a suite");
+	}
+	
+	@BeforeMethod
+	public void beforemeThod() {
+		
+		System.out.println("before method need a class");
+	}
+	
+	@BeforeTest
+	public void beforeTesting() {
+		
+		System.out.println("before test you need clear about your requiremtns");
+	}
+	
+  @Test(invocationCount=10)
+  public void f() {
+	  
+	  System.out.println("The Testing code start here");
+	  
+	  
+  }
+}
